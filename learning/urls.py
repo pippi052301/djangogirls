01330a74c446228200a2bb3ce7e5e56533f7e3a7
LeNguyenKhanhLinh <lenguyenkhanhlinh.work@gmail.com>
2026-08-10 14:30:06 +0700

    adding home.html

--- learning/urls.py
+++ learning/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
 from . import views
+
 
 app_name = "learning"
 
+
 urlpatterns = [
     path("", views.home, name="home"),
+    path("notes/create/", views.create_note, name="create_note"),
+    path("notes/", views.note_list, name="note_list"),
 ]
--- learning/views.py
+++ learning/views.py
@@ -1,23 +1,38 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Note
-from .forms import  NoteForm
-# Create your views here. 
+from .forms import NoteForm
+
 
 def home(request):
+    return render(request, "learning/home.html")
+
+
+def create_note(request):
     if request.method == "POST":
         form = NoteForm(request.POST)
+
         if form.is_valid():
             form.save()
-            
-            return redirect("note_list")
-        
+            return redirect("learning:note_list")
+
     else:
         form = NoteForm()
+
     return render(
-        request, 
+        request,
         "learning/note_form.html",
         {
             "form": form
+        }
+    )
+    
+def note_list(request):
+    notes = Note.objects.all()
+
+    return render(
+        request,
+        "learning/note_list.html",
+        {
+            "notes": notes
         }
     )
