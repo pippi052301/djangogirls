05d0a07c6f617672e24ea4797fd9cfb927884747
Ezarmono <nmq656@gmail.com>
2026-08-10 15:26:48 +0700

    Add note_list and login

--- learning/urls.py
+++ learning/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
-
 from . import views
 
 app_name = "learning"
 
 urlpatterns = [
     path("", views.home, name="home"),
-]
-urlpatterns = [
-    path('notes/new/', views.note_create, name='note_create'),
+    path("notes/new/", views.note_create, name="note_create"),
 ]
