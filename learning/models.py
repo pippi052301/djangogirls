1c4c1bb0f6e962eb4e67aeab5fc8825b8213fd0c
LeNguyenKhanhLinh <lenguyenkhanhlinh.work@gmail.com>
2026-08-10 13:52:54 +0700

    "a note"

--- learning/models.py
+++ learning/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# create a note page
+class Note(models.Model):
+    title = models.CharField(max_length=500)
+    content = models.TextField()
