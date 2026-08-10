00a0cc3046341afe0eae7da58699e3e792d3104d
jundyaian <al25131@shibaura-it.ac.jp>
2026-08-10 13:55:32 +0700

    Updated test.py
    for testcases

--- learning/tests.py
+++ learning/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HomeTest(TestCase):
+    def test_home_page(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200) #status_code = 404, 200, etc
