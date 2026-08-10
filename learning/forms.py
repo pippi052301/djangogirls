0921371828c0ce073c91c2d2e833b7dd13d66675
LeNguyenKhanhLinh <lenguyenkhanhlinh.work@gmail.com>
2026-08-10 14:13:05 +0700

    note view

--- learning/forms.py
+++ learning/forms.py
@@ -1,4 +1,4 @@
-from django.db import forms
+from django import forms
 from .models import Note
 
 class NoteForm(forms.ModelForm):
--- learning/views.py
+++ learning/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import Note
+from .forms import  NoteForm
+# Create your views here. 
 
 def home(request):
-    return HttpResponse("Hello, Django Girls!")
+    if request.method == "POST":
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+            return redirect("note_list")
+        
+    else:
+        form = NoteForm()
+    return render(
+        request, 
+        "learning/note_form.html",
+        {
+            "form": form
+        }
+    )
