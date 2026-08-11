ce5092f05d66d99337c48169845736bbec75deb1
LeNguyenKhanhLinh <lenguyenkhanhlinh.work@gmail.com>
2026-08-11 13:48:44 +0700

    adding login and logout proccess

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,3 +131,4 @@ STATIC_ROOT = BASE_DIR / 'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
