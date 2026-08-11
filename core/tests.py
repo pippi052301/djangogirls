e2f36ac85a59d2261f80135d832d0e4f833da06b
jundyaian <al25131@shibaura-it.ac.jp>
2026-08-11 09:53:46 +0700

    Tested access to pages
    ・accounts
    ・ai_engine
    ・core
    ・notes
    (・learning)

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class corePageTest(TestCase):
+    def test_core_page_opens(self):
+        response = self.client.get("/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Core Home")
