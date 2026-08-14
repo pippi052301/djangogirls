5e6888e30a7d2ea2bbbe77dc11575dac87cb7882
jundyaian <al25131@shibaura-it.ac.jp>
2026-08-14 14:55:24 +0700

    ok

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,10 +78,7 @@ TEMPLATES = [
 # ================= DATABASE =================
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    
 }
 
 
